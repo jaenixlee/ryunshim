823f47540cf0a12d87f6dbade24bc7806f798bde
jaenixlee <jaeni_lee@yahoo.com>
2023-10-25 15:45:00 -0400

    worked on work page

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -5,34 +5,45 @@ const Work = () => {
   return (
     <div className={styles.container}>
       <div className={styles.header}>
-        <span className={styles.opening1}>I’m Ryun — a designer </span>
-        <span className={styles.opening2}>striving to<br />craft with intentionality &<br />sincerity in every pixel & flow</span>
+        <p className={styles.opening1}>Hey there! I’m Ryun — </p>
+        <p className={styles.opening2}>An artist turned product designer driven by interdisciplinary collaboration. Currently @Cornell University.</p>
       </div>
 
       <div className={styles.caseStudies}>
         <p className={styles.heading}>01. CASE STUDIES</p>
         <div className={styles.caseStudiesContainer}>
           <div className={styles.caseStudiesBox} style={{ background: '#DCC8DF' }}>
-            <p className={styles.category}>Product Design, Prototyping</p>
-            <p className={styles.title}>Placeholder Text</p>
+            <p className={styles.category}>Product Design, User Testing</p>
+            <p className={styles.title}>Bridging the Gap in STEM Education</p>
           </div>
-          <div className={styles.caseStudiesBox} style={{ background: '#DCC8DF' }}>
-            <p className={styles.category}>Product Design, Prototyping</p>
-            <p className={styles.title}>Placeholder Text</p>
-          </div>
-          <div className={styles.caseStudiesBox} style={{ background: '#DCC8DF' }}>
-            <p className={styles.category}>Product Design, Prototyping</p>
-            <p className={styles.title}>Placeholder Text</p>
-          </div>
-          <div className={styles.caseStudiesBox} style={{ background: '#DCC8DF' }}>
-            <p className={styles.category}>Product Design, Prototyping</p>
-            <p className={styles.title}>Placeholder Text</p>
+          <div className={styles.caseStudiesBox} style={{ background: '#DAE9C7' }}>
+            <p className={styles.category}>User Interface, Visual Design</p>
+            <p className={styles.title}>Fostering Continuous Birding Exploration</p>
           </div>
         </div>
       </div>
 
-      <div className={styles.sideProjects}>
-        <p className={styles.heading}>02. SIDE PROJECTS</p>
+
+      <div className={styles.research}>
+        <p className={styles.heading}>02. RESEARCH</p>
+        <div className={styles.researchContainer}>
+          <div className={styles.researchLeft}>
+            <div className={styles.researchBox} style={{ background: '#D5F4F9' }}>
+              <p className={styles.category}>Co-Design, Future Technologies</p>
+              <p className={styles.title}>Mindfulness Enriched Through Creative Collaboration</p>
+            </div>
+          </div>
+          <div className={styles.researchRight}>
+            <div className={styles.researchBox} style={{ background: '#D1DAFC' }}>
+              <p className={styles.category}>LLM-Chain Technologies</p>
+              <p className={styles.title}>The Social Media Playwright: Chatbot</p>
+            </div>
+            <div className={styles.researchBox} style={{ background: '#FF8181' }}>
+              <p className={styles.category}>Co-Design, Large Language Models</p>
+              <p className={styles.title}>Co-Designing LLM Prompts with Experts</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
