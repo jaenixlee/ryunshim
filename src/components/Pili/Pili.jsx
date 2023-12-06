170c5513b18006cddd363d7e9d3fc42e62e9974b
jaenixlee <jaeni_lee@yahoo.com>
2023-12-05 22:54:52 -0500

    worked on pili

--- src/components/Pili/Pili.jsx
+++ src/components/Pili/Pili.jsx
@@ -4,6 +4,9 @@ import b from '../../assets/Pili/Class.png';
 import c from '../../assets/Pili/3.png';
 import d from '../../assets/Pili/Screenshot 2023-08-27 at 12.29 1.png';
 import charts from '../../assets/Pili/Charts.png';
+import e from '../../assets/Pili/4.png';
+import f from '../../assets/Pili/5.png';
+import g from '../../assets/Pili/6.png';
 
 
 const Pili = () => {
@@ -76,12 +79,95 @@ const Pili = () => {
           <p className={styles.specialSubHeading}>Uncovering Systemic Obstacles</p>
           <p style={{ marginTop: "0" }}>Exploratory research, including extensive secondary research and literature reviews, laid the groundwork by identifying systemic challenges that underrepresented STEM students face in academic settings.</p>
           <img style={{ position: "relative", left: "-12%", height: "auto", width: "125%" }} src={charts}></img>
-          <p>These statistics unveil the systemic challenges related to diversity and representation in STEM, which have the <mark style={{ fontStyle: "italic", fontWeight: "500", lineHeight: "2" }}>potential to directly contribute to the feelings of disconnection.</mark></p>
+          <p>These statistics unveil the systemic challenges related to diversity and representation in STEM, which have the <mark style={{ fontStyle: "italic", fontWeight: "500", lineHeight: "2", display: "inline", padding: "0.5rem 0" }}>potential to directly contribute to the feelings of disconnection.</mark></p>
           <p className={styles.specialSubHeading}>Identifying the Gaps</p>
           <p style={{ marginTop: "0" }}>To inform our design and development process, we analyzed existing solutions addressing community-building in education. GroupMe and EdStem both fall short in fostering holistic student interactions, highlighting shortcomings in their ability to cater to the multifaceted needs of STEM students.</p>
         </div>
       </div>
 
+      <div className={styles.section6}>
+        <div className={styles.edstem}>
+          <img style={{ width: "25%", height: "auto" }} src={e}></img>
+          <div className={styles.right}>
+            <h4>Edstem</h4>
+            <p>Question and answer forum for students, targeting professors and staff for communication.</p>
+            <ul>
+              <li>👍 Academic Focus</li>
+              <li>👍 Clear Communication</li>
+              <li>👍 Resource Sharing</li>
+              <li>👎 Limited Real-Time Interaction</li>
+              <li>👎 Narrow Scope/Interaction</li>
+              <li>👎 Teacher-Centric</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className={styles.groupme}>
+          <div className={styles.left}>
+            <h4>GroupMe</h4>
+            <p>A popular group messaging platform, often utilized amongst students</p>
+            <ul>
+              <li>👍 Easy group creation</li>
+              <li>👍 Shift to community focused through groups</li>
+              <li>👍 Limited Visibility- Degree of privacy</li>
+              <li>👎 Unorganized Interaction</li>
+              <li>👎 Shallow Engagement</li>
+              <li>👎 Narrow Scope/Interaction</li>
+            </ul>
+          </div>
+          <img style={{ width: "30%", height: "auto" }} src={f}></img>
+        </div>
+      </div>
+
+      <div className={styles.section7}>
+        <p>We uncovered a striking trend — <mark style={{ fontStyle: "italic", fontWeight: "500", lineHeight: "2", display: "inline", padding: "0.5rem 0" }}>educational institutions predominantly rely on generic communication and collaboration platforms for students.</mark>
+          &nbsp; This approach has resulted in the absence of a centralized, community-based platform specifically engineered to support connections,
+          leaving a noticeable gap in the educational technology landscape.</p>
+
+        <div className={styles.drillingDeeper}>
+          <h3>DRILLING DEEPER</h3>
+          <p className={styles.subheading}>Structured Interviews</p>
+          <p>We aimed to discover how these systemic challenges and limitations translated into real-life experiences through an hour-long structured interview with 8 Cornell minority undergraduate STEM students.</p>
+        </div>
+
+        <div className={styles.sampleQuestions}>
+          <p style={{ fontSize: "1.5rem", fontStyle: "italic" }}>Sample Questions</p>
+          <ol type="1" style={{ margin: "0" }}>
+            <li>So you&apos;ve successfully completed your first three weeks of the Fall semester. Could you walk me through your initial feelings when attending lectures, specifically lectures with 100+ students?</li>
+            <li>Describe a time when you are nervous about attending a large lecture. What aspects of the class make you feel uncomfortable?</li>
+            <li>How important is diversity and representation in your academic and social interactions with STEM courses?</li>
+            <li>What are some factors that go into deciding where or with whom you decide to sit next to when attending a new class?</li>
+            <li>What challenges do you face when trying to build relationships with your peers in large STEM courses?</li>
+          </ol>
+        </div>
+      </div>
+
+      <div className={styles.section8}>
+        <div className={styles.right}>
+          <div className={styles.rightSection}>
+            <h2>🐟 Small Fish in a Big Pond</h2>
+            <p>In large STEM lectures, the challenge of initiating connections can be particularly daunting because the sheer size of the class often means that students don&apos;t even know the names or faces of their classmates.</p>
+          </div>
+          <div className={styles.rightSection}>
+            <h2>🌈 Varied Expectations</h2>
+            <p>Some emphasized their desire for study partners who can help them excel academically, while others expressed the need for a social support network to navigate the challenges of STEM coursework.</p>
+          </div>
+          <div className={styles.rightSection}>
+            <h2>👯 Importance of Diversity</h2>
+            <p>Students stressed the vital importance of diversity, drawing from their experiences in team and project based STEM courses.</p>
+          </div>
+          <div className={styles.rightSection}>
+            <h2>😰 Anxiousness</h2>
+            <p>Large lectures often result in initial awkwardness and superficial conversations, coupled with the challenge of finding like-minded peers and initiating meaningful dialogues.</p>
+          </div>
+        </div>
+        <img src={g}></img>
+      </div>
+
+      <div className={styles.section9}>
+        <p>Structured interviews with Cornell minority STEM students provided firsthand accounts and humanized our understanding, revealing the need for a versatile platform that nurtures both academic and social connections.</p>
+      </div>
+      
     </div>
   )
 }
